69f83f5b3cd94d441ce02c0118d56f2f1f96be1a
Amjed Omar <40240660+amjedomar@users.noreply.github.com>
2023-01-22 16:45:06 +0200

    completed: Stage 4/5: Check names according to PEP8

--- main.py
+++ main.py
@@ -15,21 +15,26 @@ class Utils:
 
         return None
 
+    @staticmethod
+    def form_msg(cond, msg):
+        return msg if cond else None
+
 
 class Checkers:
-    preceding_empty_lines = 0
+    def __init__(self):
+        self.preceding_empty_lines = 0
 
     @staticmethod
     def check_issue_s001(line):
-        return len(line.rstrip()) > 79
+        return Utils.form_msg(len(line.rstrip()) > 79, 'Too long')
 
     @staticmethod
     def check_issue_s002(line):
         if re.match(r"\s*$", line):
-            return False
+            return None
 
         ind = len(line) - len(line.lstrip())
-        return ind % 4 != 0
+        return Utils.form_msg(ind % 4 != 0, 'Indentation is not a multiple of four')
 
     @staticmethod
     def check_issue_s003(line):
@@ -41,59 +46,85 @@ class Checkers:
 
         for match in matches:
             if match.group() == ';':
-                return True
+                return 'Unnecessary semicolon'
 
-        return False
+        return None
 
     @staticmethod
     def check_issue_s004(line):
         comment_idx = Utils.find_comment(line.rstrip())
         if comment_idx is None or comment_idx == 0:
-            return False
+            return None
         before_comment = line[comment_idx - 2: comment_idx]
-        return before_comment != '  '
+        is_issue = before_comment != '  '
+        return Utils.form_msg(is_issue, 'At least two spaces required before inline comments')
 
     @staticmethod
     def check_issue_s005(line):
         comment_idx = Utils.find_comment(line)
         if comment_idx is None:
-            return False
+            return None
 
         comment_line = line[comment_idx + 1:]
         pattern = 'todo'
+        is_issue = re.search(pattern, comment_line, flags=re.I) is not None
+        return Utils.form_msg(is_issue, 'TODO found')
 
-        return re.search(pattern, comment_line, flags=re.I) is not None
+    def check_issue_s006(self, line):
+        if line.rstrip() != '':
+            if self.preceding_empty_lines > 2:
+                self.preceding_empty_lines = 0
+                return 'More than two blank lines used before this line'
+            self.preceding_empty_lines = 0
+        else:
+            self.preceding_empty_lines += 1
+
+        return None
 
     @staticmethod
-    def check_issue_s006(line):
-        if line.rstrip() != '':
-            if Checkers.preceding_empty_lines > 2:
-                Checkers.preceding_empty_lines = 0
-                return True
-            Checkers.preceding_empty_lines = 0
-        else:
-            Checkers.preceding_empty_lines += 1
+    def check_issue_s007(line):
+        pattern = r"\s*(def|class)\b(\s+)"
+        match = re.match(pattern, line)
+        if match:
+            keyword, spaces = match.group(1), match.group(2)
+            if len(spaces) > 1:
+                return f"Too many spaces after '{keyword}'"
+        return None
 
-        return False
+    @staticmethod
+    def check_issue_s008(line):
+        line_pattern = r"\s*class\b\s+([\S\w]+?)\b"
+        match = re.match(line_pattern, line)
+        if match:
+            name_pattern = r"[A-Z]+[A-Za-z\d]*$"
+            class_name = match.group(1)
+            is_issue = re.match(name_pattern, class_name) is None
+            return Utils.form_msg(is_issue, f"Class name 'user' should use CamelCase")
 
-
-codes = {
-    's001': 'Too long',
-    's002': 'Indentation is not a multiple of four',
-    's003': 'Unnecessary semicolon',
-    's004': 'At least two spaces required before inline comments',
-    's005': 'TODO found',
-    's006': 'More than two blank lines used before this line'
-}
+    @staticmethod
+    def check_issue_s009(line):
+        line_pattern = r"\s*def\b\s+([\S\w]+?)\b"
+        match = re.match(line_pattern, line)
+        if match:
+            name_pattern = r"[_a-z]+[_a-z\d]*$"
+            fn_name = match.group(1)
+            is_issue = re.match(name_pattern, fn_name) is None
+            return Utils.form_msg(is_issue, f"Function name {fn_name} should use snake_case")
 
 
 def lint_file(file_path):
     with open(file_path) as f:
+        checkers = Checkers()
         for num, line in enumerate(f.readlines(), start=1):
-            for code, msg in codes.items():
-                check_issue_fn = getattr(Checkers, f'check_issue_{code}')
-                if check_issue_fn(line):
-                    print(f'{file_path}: Line {num}: {code} {msg}')
+            for instance_attr in dir(checkers):
+                pattern = r"check_issue_(s\d{3})$"
+                match = re.match(pattern, instance_attr)
+                if match:
+                    check_issue_fn = getattr(checkers, match.group())
+                    code = match.group(1)
+                    issue_msg = check_issue_fn(line)
+                    if issue_msg:
+                        print(f'{file_path}: Line {num}: {code} {issue_msg}')
 
 
 def lint_directory(directory_path):
